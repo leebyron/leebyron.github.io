d28d513742c01fddd7ea87b34fdd32d217e490b5
Lee Byron <lee@leebyron.com>
2019-12-03 22:59:22 -0800

    Replace next-images with custom loader

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
-const withImages = require('next-images')
-
-module.exports = withImages({
-  inlineImageLimit: 1024,
-});
+module.exports = {
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(jpe?g|png|svg|gif|ico|webp|woff2?)$/,
+      use: [
+        {
+          loader: require.resolve('url-loader'),
+          options: {
+            esModule: false,
+            limit: 1024,
+            publicPath: `/_next/static/images/`,
+            outputPath: `${options.isServer ? '../' : ''}static/images/`,
+            name: '[name]-[hash].[ext]'
+          }
+        }
+      ]
+    })
+    return config
+  }
+}
