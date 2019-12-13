7c49f5445a11de1b7c813378fb92b58afce5476c
Lee Byron <lee@leebyron.com>
2019-12-12 23:55:11 -0800

    Add 6th talk and create 2wide grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,33 +36,9 @@ export default () => (
     <Header />
     <Page>
       <style jsx>{`
-        .talk {
-          display: block;
-          margin-bottom: 2em;
-        }
-
-        .talk img {
-          width: 100%;
-          display: block;
-          margin-bottom: 0.5em;
-        }
-
-        @media screen and (min-width: 768px) and (min-height: 500px) {
+        @media screen and (min-width: 768px) {
           p {
             max-width: 55ch;
-          }
-
-          .talk {
-            min-width: 24px;
-            width: calc(33.3% - 2ch);
-            margin-left: 2ch;
-          }
-
-          .talks {
-            display: flex;
-            flex-wrap: wrap;
-            margin-bottom: -2em;
-            margin-left: -2ch;
           }
         }
 
@@ -81,33 +57,76 @@ export default () => (
         React, Immutable.js, Flow, Relay, Dataloader, and more.
       </p>
       <h2>Speaking</h2>
-      <div className="talks">
-        <a className="talk" href="https://youtu.be/vG8WpLr6y_U" target="_blank">
-          <img src={require('../assets/talks/1999.jpg')} />
-          Program like it's 1999
-        </a>
-        <a className="talk" href="https://youtu.be/VjHWkBr3tjI" target="_blank">
-          <img src={require('../assets/talks/graphql-history.jpg')} />
-          Brief history of GraphQL
-        </a>
-        <a className="talk" href="https://youtu.be/oTcDmnAXZ4E" target="_blank">
-          <img src={require('../assets/talks/idea-architecture.jpg')} />
-          The IDEA Architecture
-        </a>
-        <a className="talk" href="https://youtu.be/Oh5oC98ztvI" target="_blank">
-          <img src={require('../assets/talks/data-language.jpg')} />
-          Designing a data language
-        </a>
-        <a className="talk" href="https://youtu.be/I7IdS-PbEgI" target="_blank">
-          <img src={require('../assets/talks/immutable-data.jpg')} />
-          Immutable data in React
-        </a>
-      </div>
+      <Talks />
       <h2>Articles & Essays</h2>
       <AllArticlesList />
     </Page>
   </Body>
 )
+
+function Talks() {
+  return (
+    <div className="talks">
+      <style jsx>{`
+        .talk {
+          display: block;
+          margin-bottom: 2em;
+        }
+
+        .talk img {
+          width: 100%;
+          display: block;
+          margin-bottom: 0.5em;
+        }
+
+        @media screen and (min-width: 350px) {
+          .talks {
+            display: flex;
+            flex-wrap: wrap;
+            margin-bottom: -2em;
+            margin-left: -2ch;
+          }
+
+          .talk {
+            min-width: 24px;
+            width: calc(50% - 2ch);
+            margin-left: 2ch;
+          }
+        }
+
+        @media screen and (min-width: 700px) {
+          .talk {
+            width: calc(33.3% - 2ch);
+          }
+        }
+      `}</style>
+      <a className="talk" href="https://youtu.be/vG8WpLr6y_U" target="_blank">
+        <img src={require('../assets/talks/1999.jpg')} />
+        Program like it's 1999
+      </a>
+      <a className="talk" href="https://youtu.be/VjHWkBr3tjI" target="_blank">
+        <img src={require('../assets/talks/graphql-history.jpg')} />
+        Brief history of GraphQL
+      </a>
+      <a className="talk" href="https://youtu.be/oTcDmnAXZ4E" target="_blank">
+        <img src={require('../assets/talks/idea-architecture.jpg')} />
+        The IDEA Architecture
+      </a>
+      <a className="talk" href="https://youtu.be/Oh5oC98ztvI" target="_blank">
+        <img src={require('../assets/talks/data-language.jpg')} />
+        Designing a data language
+      </a>
+      <a className="talk" href="https://youtu.be/WQLzZf34FJ8" target="_blank">
+        <img src={require('../assets/talks/exploring-graphql.jpg')} />
+        Exploring GraphQL
+      </a>
+      <a className="talk" href="https://youtu.be/I7IdS-PbEgI" target="_blank">
+        <img src={require('../assets/talks/immutable-data.jpg')} />
+        Immutable data in React
+      </a>
+    </div>
+  )
+}
 
 function Page({ children }: { children: ReactNode }) {
   return (
